3b67863f14acd5296587ba5844bc0f3a1a36fe6d
Simran Mandal <simranmandal0211@gmail.com>
2023-09-07 16:23:26 +0530

    Add controller modules

---
+++ controller/options_controller.js
@@ -0,0 +1,64 @@
+const Question = require('../models/questions');
+const Option = require('../models/options');
+
+module.exports.create = async function(request, respond){
+    // in this we will create the options to the od given question
+    console.log(request.body, request.param.id);
+    const opt = await Option.create({
+        option: request.body.content,
+        question: request.params.id,
+    })
+    // it is for adding the vote to option of the id that is given by mongodb by update quesry and using the string interpolition
+    const updateOpt = await Option.findByIdAndUpdate(opt._id, {"add_vote": `http://localhost:1024/api/v1/options/${opt._id}/add_vote`});
+
+    updateOpt.save();
+    // now searching thee question so that we can append the option in question---> option array
+    const ques = await Question.findById(request.params.id);
+
+    if(ques){
+        ques.options.push(updateOpt);
+        ques.save();
+        console.log(ques);
+        respond.send(ques);
+    }
+    else{
+        respond.send('question does not exists');
+    }
+}
+
+module.exports.add_vote = async function(request, respond){
+    // in this votes will be added to the particular option of the question
+    console.log(request.params.id);
+    // this the increment query in which the vote is increment by one
+    const opt = await Option.findByIdAndUpdate(request.params.id, { $inc: { vote: 1 }})
+        if(opt){
+            await opt.save();
+            console.log(opt);
+            respond.send(opt);
+        }
+        // handling the bad requests
+        else{
+            respond.senf('option does not exists');
+        }
+
+}
+
+
+module.exports.delete = async function(request, respond){
+    // delete the id option
+    console.log('id', request.params.id);
+    const opt = await Option.findById(request.params.id);
+    if(opt){
+        const quesId = opt.question;
+        // finding the question to which the option is deleted and removing the option from ots option array 
+        const ques = await Question.findByIdAndUpdate(quesId, { $pull: {options: request.params.id} } );
+            // now absolutely deleting tht option
+        await Option.findByIdAndDelete(request.params.id);
+
+        console.log(ques);
+        respond.send('option deleted');
+    }
+    else{
+        respond.send('id not exists');
+    }
+}
--- controller/questions_controller.js
+++ controller/questions_controller.js
@@ -1,3 +1,61 @@
+const Question = require('../models/questions');
+const Option = require('../models/options');
+
+
 module.exports.home = function(request, respond){
     return respond.end('<h1>Express is up for Polling system API!</h1>');
+}
+
+module.exports.create = async function(request, respond){
+    console.log(request.url);
+    console.log(request.body);
+
+    await Question.create(Request.body, function(err, ques){
+        if(err){
+            console.log('error in creating the question schema', err);
+        }
+
+        console.log(ques);
+        respond.send(ques);
+    })
+}
+
+module.exports.showDetails = async function(request, respond){
+    console.log(request.params.id);
+    const ques = await Question.findById(request.params.id).populate('options');
+
+    if(ques){
+        respond.send(ques);
+    }
+    // handling the bad requests if that id does not exists
+    else{
+        respond.send('id does not exits');
+    }
+
+    // in this the details about the question is displayed
+}
+
+
+module.exports.deleteQues = async function(request, respond){
+    const ques = await Question.findById(request.params.id).clone().catch(function(err){
+        console.log(err)});
+
+    if(ques){
+        // delete all the option of the option db having the question id as the request.params.id
+
+        await Question.deleteOne(request.params.id).clone().catch(function(err){
+            console.log(err)});
+
+        // deleting all the option of that question
+        await Option.deleteMany({ question: request.params.id}).clone().catch(function(err){
+            console.log(err)
+        })
+
+        respond.send("ques deleted");
+    }
+    //  if the at question of the given id does not exists then just sending a message
+    else{
+        respond.send('question does not exists');
+    }
+    
 }
